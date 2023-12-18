df8630ea392e916183114cb96f7f196fffff069a
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-19 00:16:20 +0100

    learning Javascript arrays

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 // Arrays
+//==========Array is a data structure in Javascript
+
+const myArrays = [];
+
+//add elements to an array
+myArrays[0] = "Manny";
+myArrays[1] = 1001;
+myArrays[2] = false;
+
+// refer to an array
+console.log(myArrays);
+
+//lenght property
+console.log(myArrays.length);
+
+//last element in a array
+console.log(myArrays[myArrays.length - 1]);
